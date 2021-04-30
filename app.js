30c7dae7acad5a26fac38b8990d8681f824a3675
Noam Annenberg <noam.annenberg@gmail.com>
2021-04-29 21:21:03 -0400

    set up handlebars and add a tempalte for the home page

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.get('/', function(request, response) {
-    response.send('Hello World');
+    response.render('index', {name: 'Noam'});
 });
 
 app.listen(port, function() {
