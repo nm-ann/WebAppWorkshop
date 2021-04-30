d86d6fbdbd08241b94a74daf78337df3ca43255c
Noam Annenberg <noam.annenberg@gmail.com>
2021-04-29 21:25:06 -0400

    add notes page

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.get('/', function(request, response) {
     response.render('index', {name: 'Noam'});
 });
 
+app.get('/notes', function(request, response) {
+    response.render('notes', {
+        "notes": [{id: 1, title: 'This is a note.', body: 'This is the note body.'}]
+    })
+});
+
 app.listen(port, function() {
     console.log('Server starting on http://localhost:' + port);
 });
