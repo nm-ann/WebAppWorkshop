8fa79dcce7fe42640d6e4cd7ccf15bdaa22e3f87
Noam Annenberg <noam.annenberg@gmail.com>
2021-04-29 21:28:24 -0400

    add note form and /add_note route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(request, response) {
     response.render('index', {name: 'Noam'});
@@ -15,6 +17,10 @@ app.get('/notes', function(request, response) {
     response.render('notes', {
         "notes": [{id: 1, title: 'This is a note.', body: 'This is the note body.'}]
     })
+});
+
+app.post('/add_note', function(request, response) {
+    console.log(request.body);
 });
 
 app.listen(port, function() {
